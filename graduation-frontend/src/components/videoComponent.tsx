eab5bde04c5336445edc5ea6ae2008f6bc9e828b
Licodeao <1019805199@qq.com>
2024-03-11 16:25:45 +0800

    fix: video component

--- graduation-frontend/src/components/videoComponent.tsx
+++ graduation-frontend/src/components/videoComponent.tsx
@@ -1,50 +1,20 @@
 import { Skeleton } from "@mui/material";
 import { useEffect, useState } from "react";
+import videoSource from "@/assets/img/vedio.mp4";
 
 const VideoComponent = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
   useEffect(() => {
-    const video = document.getElementById("registerVideo") as HTMLElement;
-
-    if (!video) return;
-
-    const handleLoaded = () => {
-      console.log("视频加载完毕");
-      setIsLoading(true);
-    };
-
-    const handleError = () => {
-      console.log("视频加载出错");
-      setIsLoading(false);
-    };
-
-    video?.addEventListener("loadeddata", handleLoaded);
-    video?.addEventListener("error", handleError);
-
-    return () => {
-      video.removeEventListener("loadeddata", handleLoaded);
-      video.removeEventListener("error", handleError);
-    };
-  }, []);
+    setIsLoading(true);
+  }, [isLoading]);
 
   return (
     <>
       {isLoading ? (
-        <div>
-          <video
-            className="w-full h-full object-cover"
-            autoPlay
-            muted
-            loop
-            id="registerVideo"
-          >
-            <source
-              src="https://typora-licodeao.oss-cn-guangzhou.aliyuncs.com/typoraImg/vedio.mp4"
-              type="video/mp4"
-            />
-          </video>
-        </div>
+        <video className="w-full h-full object-cover" autoPlay muted loop>
+          <source src={videoSource} type="video/mp4" />
+        </video>
       ) : (
         <Skeleton
           variant="rectangular"
