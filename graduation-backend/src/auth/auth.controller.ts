b96f8a2c97c76ca3f7087034a5b26460fd36127e
Licodeao <1019805199@qq.com>
2024-03-12 16:56:57 +0800

    feat(Oauth): twitter login

--- graduation-backend/src/auth/auth.controller.ts
+++ graduation-backend/src/auth/auth.controller.ts
@@ -1,32 +1,59 @@
-import { Controller, Get, Query, Redirect } from '@nestjs/common';
+import { Controller, Get, Query, Redirect, Res } from '@nestjs/common';
+import axios from 'axios';
+import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
+
 import { AuthService } from './auth.service';
 import { OauthDto } from './dto/oatuh.dto';
-import axios from 'axios';
 
+type TwitterTokenResponse = {
+  token_type: 'bearer';
+  expires_in: 7200;
+  access_token: string;
+  scope: string;
+};
+
+interface TwiterUser {
+  id: string;
+  name: string;
+  username: string;
+}
 @Controller('oauth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Get('github')
   @Redirect('/', 301)
   async getGithubOAuth(@Query() query: OauthDto) {
-    const { code } = query;
-    const { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, GITHUB_REDIRECT_URI } =
-      process.env;
-    console.log(code, GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET);
-    const config = {
-      method: 'POST',
-      url: `https://github.com/login/oauth/access_token?client_id=${GITHUB_CLIENT_ID}&
-      client_secret=${GITHUB_CLIENT_SECRET}&code=${code}&redirect_uri=${GITHUB_REDIRECT_URI}`,
-      headers: {
-        'Content-Type': 'application/json',
-        accept: 'application/json',
-      },
-    };
+    try {
+      const { code } = query;
+      const GITHUB_CLIENT_ID =
+        this.configService.get<string>('GITHUB_CLIENT_ID');
+      const GITHUB_CLIENT_SECRET = this.configService.get<string>(
+        'GITHUB_CLIENT_SECRET',
+      );
+      const GITHUB_REDIRECT_URI = this.configService.get<string>(
+        'GITHUB_REDIRECT_URI',
+      );
+      const config = {
+        method: 'POST',
+        url: `https://github.com/login/oauth/access_token?client_id=${GITHUB_CLIENT_ID}&
+        client_secret=${GITHUB_CLIENT_SECRET}&code=${code}&redirect_uri=${GITHUB_REDIRECT_URI}`,
+        headers: {
+          'Content-Type': 'application/json',
+          accept: 'application/json',
+        },
+      };
 
-    return axios.request(config).then((res) => {
-      console.log(res.data);
-    });
+      return axios.request(config).then((res) => {
+        console.log(res);
+      });
+    } catch (e) {
+      console.log('🚀 ~ AuthController ~ getGithubOAuth ~ e:', e);
+    }
 
     // return this.authService.validateAuth();
   }
@@ -68,5 +95,96 @@ export class AuthController {
           console.log(res.data);
         });
     });
+  }
+
+  @Get('twitter')
+  @Redirect('/', 301)
+  async getTwitterOAuth(@Query() query: OauthDto, @Res() res: Response) {
+    const { code } = query;
+    const TWITTER_CLIENT_ID =
+      this.configService.get<string>('TWITTER_CLIENT_ID');
+    const TWITTER_CLIENT_SECRET = this.configService.get<string>(
+      'TWITTER_CLIENT_SECRET',
+    );
+    const TWITTER_REDIRECT_URI = this.configService.get<string>(
+      'TWITTER_REDIRECT_URI',
+    );
+    console.log('🚀 ~ TWITTER_CLIENT_SECRET:', TWITTER_CLIENT_SECRET);
+    console.log('🚀 ~ TWITTER_CLIENT_ID:', TWITTER_CLIENT_ID);
+
+    const config = {
+      client_id: `${TWITTER_CLIENT_ID}`,
+      code_verifier: '8KxxO-RPl0bLSxX5AWwgdiFbMnry_VOKzFeIlVA7NoA',
+      redirect_uri: TWITTER_REDIRECT_URI,
+      grant_type: 'authorization_code',
+    };
+
+    const basicAuthToken = Buffer.from(
+      `${TWITTER_CLIENT_ID}:${TWITTER_CLIENT_SECRET}`,
+      'utf-8',
+    ).toString('base64');
+    console.log('🚀 ~ basicAuthToken:', basicAuthToken);
+
+    async function getTwitterOAuthToken(code: string) {
+      try {
+        const res = await axios.post<TwitterTokenResponse>(
+          'https://api.twitter.com/2/oauth2/token',
+          new URLSearchParams({ ...config, code }).toString(),
+          {
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded',
+              Authorization: `Basic ${basicAuthToken}`,
+            },
+          },
+        );
+        return res.data;
+      } catch (e) {
+        // throw new Error(e);
+        console.log(e);
+      }
+    }
+
+    async function getTwitterUserInfo(
+      accessToken: string,
+    ): Promise<TwiterUser | null> {
+      try {
+        const res = await axios.get<{ data: TwiterUser }>(
+          'https://api.twitter.com/2/users/me',
+          {
+            headers: {
+              'Content-type': 'application/json',
+              Authorization: `Bearer ${accessToken}`,
+            },
+          },
+        );
+        {
+          return res.data.data ?? null;
+        }
+      } catch (e) {
+        throw new Error(e);
+      }
+    }
+
+    const TwitterAccessToken = await getTwitterOAuthToken(code);
+    console.log(
+      '🚀 ~ AuthController ~ getTwitterOAuth ~ TwitterAccessToken:',
+      TwitterAccessToken,
+    );
+
+    if (!TwitterAccessToken) {
+      return res.redirect('http://localhost:5173');
+    }
+
+    const TwitterUserInfo = await getTwitterUserInfo(
+      TwitterAccessToken.access_token,
+    );
+    console.log(
+      '🚀 ~ AuthController ~ getTwitterOAuth ~ TwitterUserInfo:',
+      TwitterUserInfo,
+    );
+
+    if (!TwitterUserInfo) {
+      return res.redirect('http://localhost:5173');
+    }
   }
 }
--- graduation-frontend/src/provider/auth.tsx
+++ graduation-frontend/src/provider/auth.tsx
@@ -12,13 +12,29 @@ const AuthProvider = ({ children }) => {
     VITE_GITHUB_REDIRECT_URI,
     VITE_GITEE_CLIENT_ID,
     VITE_GITEE_REDIRECT_URI,
+    VITE_TWITTER_CLIENT_ID,
+    VITE_TWITTER_REDIRECT_URI,
   } = import.meta.env;
 
   const loginWithGithub = () => {
     window.location.href = `https://github.com/login/oauth/authorize?client_id=${VITE_GITHUB_CLIENT_ID}&redirect_uri=${VITE_GITHUB_REDIRECT_URI}&response_type=code&state=1`;
   };
 
-  const loginWithTwitter = () => {};
+  const loginWithTwitter = () => {
+    const options = {
+      redirect_uri: VITE_TWITTER_REDIRECT_URI,
+      client_id: VITE_TWITTER_CLIENT_ID,
+      state: "state",
+      response_type: "code",
+      code_challenge: "y_SfRG4BmOES02uqWeIkIgLQAlTBggyf_G7uKT51ku8",
+      code_challenge_method: "S256",
+      scope: ["users.read", "tweet.read", "follows.read", "follows.write"].join(
+        " "
+      ),
+    };
+    const qs = new URLSearchParams(options).toString();
+    window.location.href = `https://twitter.com/i/oauth2/authorize?${qs}`;
+  };
 
   const loginWithGitee = () => {
     window.location.href = `https://gitee.com/oauth/authorize?client_id=${VITE_GITEE_CLIENT_ID}&redirect_uri=${VITE_GITEE_REDIRECT_URI}&response_type=code
