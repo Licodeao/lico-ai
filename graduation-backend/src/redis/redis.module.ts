54203754376fb79dea29a73519c0968303fd722c
Licodeao <1019805199@qq.com>
2024-03-19 17:34:15 +0800

    fix: add redis into .env

--- graduation-backend/src/redis/redis.module.ts
+++ graduation-backend/src/redis/redis.module.ts
@@ -1,6 +1,6 @@
 import { Global, Module } from '@nestjs/common';
-import { RedisService } from './redis.service';
 import { createClient } from 'redis';
+import { RedisService } from './redis.service';
 
 @Global()
 @Module({
@@ -11,8 +11,8 @@ import { createClient } from 'redis';
       async useFactory() {
         const client = createClient({
           socket: {
-            host: 'localhost',
-            port: 6379,
+            host: process.env.REDIS_HOST,
+            port: process.env.REDIS_PORT as unknown as number,
           },
         });
         await client.connect();
