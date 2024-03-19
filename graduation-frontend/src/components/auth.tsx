c76137ccb1b25cee8115ce280ee00800a11369fb
Licodeao <1019805199@qq.com>
2024-03-19 17:07:55 +0800

    feat(form): finished

--- graduation-frontend/src/components/auth.tsx
+++ graduation-frontend/src/components/auth.tsx
@@ -1,9 +1,10 @@
-import { type FC, type ReactNode } from "react";
+import { useState, useMemo, type FC, type ReactNode } from "react";
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
 
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import { useFormControl } from "@mui/material/FormControl";
 
 interface IProps {
   children?: ReactNode;
@@ -18,6 +19,7 @@ interface FormValue {
 }
 
 const AuthComponent: FC<IProps> = ({ btnText, tipPrefix, tipSuffix, url }) => {
+  const [confirmLogin, setConfirmLogin] = useState<boolean>(false);
   const { handleSubmit, register, formState } = useForm<FormValue>({
     defaultValues: {
       email: "",
@@ -29,6 +31,21 @@ const AuthComponent: FC<IProps> = ({ btnText, tipPrefix, tipSuffix, url }) => {
 
   const onSubmit = (data: FormValue) => {
     console.log(data);
+    setConfirmLogin(true);
+  };
+
+  const HelperText = () => {
+    const { focused } = useFormControl() || {};
+
+    const helperText = useMemo(() => {
+      if (focused) {
+        return "focused";
+      }
+
+      return "请在您的邮箱查看我发的邮件!";
+    }, [focused]);
+
+    return <div className="text-[12px] text-[#85C027] my-2">{helperText}</div>;
   };
 
   return (
@@ -40,26 +57,41 @@ const AuthComponent: FC<IProps> = ({ btnText, tipPrefix, tipSuffix, url }) => {
         noValidate
         onSubmit={handleSubmit(onSubmit)}
       >
-        <TextField
-          id="outlined-basic"
-          label="输入邮箱"
-          type="email"
-          variant="outlined"
-          sx={{
-            width: "100%",
-            marginBottom: "10px",
-          }}
-          error={!!errors.email}
-          helperText={errors.email?.message}
-          required
-          {...register("email", {
-            required: "请输入邮箱!",
-            pattern: {
-              value: emailRegExp,
-              message: "邮箱格式不正确，请重新输入!",
-            },
-          })}
-        />
+        {!confirmLogin ? (
+          <TextField
+            id="outlined-basic"
+            label="输入邮箱"
+            type="email"
+            variant="outlined"
+            sx={{
+              width: "100%",
+              marginBottom: "10px",
+            }}
+            error={!!errors.email}
+            helperText={errors.email?.message}
+            required
+            {...register("email", {
+              required: "请输入邮箱!",
+              pattern: {
+                value: emailRegExp,
+                message: "邮箱格式不正确，请重新输入!",
+              },
+            })}
+          />
+        ) : (
+          <>
+            <TextField
+              id="outlined-basic"
+              type="email"
+              label="请输入邮箱验证码"
+              variant="outlined"
+              sx={{
+                width: "100%",
+              }}
+            />
+            <HelperText />
+          </>
+        )}
 
         <Button
           variant="contained"
@@ -72,13 +104,23 @@ const AuthComponent: FC<IProps> = ({ btnText, tipPrefix, tipSuffix, url }) => {
         </Button>
       </form>
 
-      <div className="text-[12px]">
-        <p>
-          {tipPrefix}{" "}
-          <Link to={url} className="text-blue-500 no-underline">
-            {tipSuffix} {">"}
-          </Link>
-        </p>
+      <div className="text-[12px] w-full flex justify-center items-center">
+        {!confirmLogin ? (
+          <p>
+            {tipPrefix}{" "}
+            <Link to={url} className="text-blue-500 no-underline">
+              {tipSuffix} {">"}
+            </Link>
+          </p>
+        ) : (
+          <div className="w-3/4 flex justify-around items-center gap-36">
+            <div className="text-[#2E9CEE] cursor-pointer">更改邮箱</div>
+            <div className="flex justify-center items-center gap-1">
+              没有收到验证码?{" "}
+              <a className="text-[#2E9CEE] cursor-pointer">重发</a>
+            </div>
+          </div>
+        )}
       </div>
     </>
   );
