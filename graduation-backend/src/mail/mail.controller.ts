fde6b183f50449a5ea2abaf54b256e3682e9648d
Licodeao <1019805199@qq.com>
2024-03-21 21:41:03 +0800

    feat(Email): use redis to store email verification code

--- graduation-backend/src/mail/mail.controller.ts
+++ graduation-backend/src/mail/mail.controller.ts
@@ -1,13 +1,19 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Inject, Query } from '@nestjs/common';
 import { MailService } from './mail.service';
+import { RedisService } from 'src/redis/redis.service';
 
 @Controller('mail')
 export class MailController {
   constructor(private readonly mailService: MailService) {}
 
+  @Inject()
+  private redisService: RedisService;
+
   @Get('code')
   async sendEmailCode(@Query('address') address) {
     const code = Math.random().toString().slice(2, 8);
+
+    await this.redisService.set(`${address}_email_code`, code, 60 * 5);
 
     await this.mailService.sendMail({
       to: address,
--- graduation-backend/src/redis/redis.service.ts
+++ graduation-backend/src/redis/redis.service.ts
@@ -6,6 +6,18 @@ export class RedisService {
   @Inject('REDIS_CLIENT')
   private redisClient: RedisClientType;
 
+  async get(key: string) {
+    return await this.redisClient.get(key);
+  }
+
+  async set(key: string, value: string | number, ttl?: number) {
+    await this.redisClient.set(key, value);
+
+    if (ttl) {
+      await this.redisClient.expire(key, ttl);
+    }
+  }
+
   async getList(key: string) {
     return this.redisClient.lRange(key, 0, -1);
   }
