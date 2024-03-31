2122134b6c2d41114b67ed0db0c91c895d1b5b05
Licodeao <1019805199@qq.com>
2024-03-31 18:18:07 +0800

    feat(Edit): element page finished

--- graduation-frontend/src/pages/edit/views/element.tsx
+++ graduation-frontend/src/pages/edit/views/element.tsx
@@ -5,7 +5,19 @@ interface IProps {
 }
 
 const EditElement: FC<IProps> = () => {
-  return <div>EditElement</div>;
+  return (
+    <div className="p-6 flex flex-col gap-8 font-semibold text-[#1A2033]">
+      <span className="text-[18px]">添加元素</span>
+      <div className="flex flex-col gap-8">
+        <div className="flex flex-row justify-between items-center">
+          <h4 className="font-normal">Emoji</h4>
+          <span className="text-xs text-[#9194A5] font-normal hover:text-[#72C1FD]">
+            查看更多
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default EditElement;
