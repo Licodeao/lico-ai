8ea1d341c93713b11ae9ff2d75cbbfdf42c92efd
Licodeao <1019805199@qq.com>
2024-04-01 23:48:59 +0800

    feat(Edit): FFmpeg finished

--- graduation-frontend/src/pages/edit/components/ffmpeg.tsx
+++ graduation-frontend/src/pages/edit/components/ffmpeg.tsx
@@ -6,8 +6,22 @@ interface IProps {
 
 const Ffmpeg: FC<IProps> = () => {
   return (
-    <div className="w-full h-40 border-t-[1px] border-t-[#E1E1E3] bg-[#FFFFFF]">
-      Ffmpeg
+    <div className="w-full h-40 border-t-[1px] border-t-[#E1E1E3] bg-[#FFFFFF] flex flex-col">
+      <div className="w-full h-14 flex flex-row justify-between items-center">
+        <div className="flex flex-row justify-center items-center gap-2">
+          <div>split</div>
+          <div>voicer</div>
+        </div>
+        <div className="w-1/2 flex-1 flex flex-row justify-center items-center gap-2">
+          <div>operator</div>
+          <div>time</div>
+        </div>
+        <div className="flex flex-row justify-center items-center gap-2">
+          <div>sclae</div>
+          <div>setting</div>
+        </div>
+      </div>
+      <div className="flex-1 bg-black">Timeline</div>
     </div>
   );
 };
