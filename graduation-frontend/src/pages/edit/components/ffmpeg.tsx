876d8dc56bb5db02838c755d2c6d1c6f6e8ed865
Licodeao <1019805199@qq.com>
2024-04-02 21:45:40 +0800

    feat(Edit): ffmpeg operator header

--- graduation-frontend/src/pages/edit/components/ffmpeg.tsx
+++ graduation-frontend/src/pages/edit/components/ffmpeg.tsx
@@ -1,6 +1,6 @@
-import type { FC, ReactNode } from "react";
+import { useState, type FC, type ReactNode } from "react";
 import PauseSvg from "@/assets/img/pause.svg";
-import PlaySvg from "@/assets/img/start.svg";
+// import PlaySvg from "@/assets/img/start.svg";
 import SkipNextSvg from "@/assets/img/skip-next.svg";
 import SkipBackSvg from "@/assets/img/skip-back.svg";
 import VoiceoverSvg from "@/assets/img/voiceover.svg";
@@ -14,40 +14,63 @@ interface IProps {
 }
 
 const Ffmpeg: FC<IProps> = () => {
+  const [sliderValue, setSliderValue] = useState<number>(0);
   return (
     <div className="w-full h-40 border-t-[1px] border-t-[#E1E1E3] bg-[#FFFFFF] flex flex-col">
-      <div className="w-full h-14 flex flex-row justify-between items-center">
+      <div className="w-full h-14 flex flex-row justify-between items-center px-4">
         <div className="flex flex-row justify-center items-center gap-2">
-          <div>
+          <div className="flex flex-row justify-center items-center gap-2 p-2 rounded-lg hover:bg-[#F7F7F8] hover:cursor-pointer">
             <img src={SplitSvg} alt="split" />
-            <span>切割</span>
+            <span className="text-[#18191B] text-sm">切割</span>
           </div>
-          <div>
+          <div className="flex flex-row justify-center items-center gap-2 p-2 rounded-lg hover:bg-[#F7F7F8] hover:cursor-pointer">
             <img src={VoiceoverSvg} alt="voiceover" />
-            <span>录音</span>
+            <span className="text-[#18191B] text-sm">画外音</span>
           </div>
         </div>
-        <div className="w-1/2 flex-1 flex flex-row justify-center items-center gap-2">
-          <div>
-            <img src={SkipBackSvg} alt="skip-back" />
-            <img src={PauseSvg} alt="pause" />
+        <div className="w-1/2 flex-1 flex flex-row justify-center items-center gap-3">
+          <div className="flex flex-row justify-center items-center gap-2">
+            <img
+              src={SkipBackSvg}
+              alt="skip-back"
+              className="bg-[#F7F7F8] rounded-full p-1 hover:bg-[#EEEEF0] hover:cursor-pointer"
+            />
+            <img
+              src={PauseSvg}
+              alt="pause"
+              className="bg-[#F7F7F8] rounded-full p-2 hover:bg-[#EEEEF0] hover:cursor-pointer"
+            />
             {/* <img src={PlaySvg} alt="play" /> */}
-            <img src={SkipNextSvg} alt="skip-next" />
+            <img
+              src={SkipNextSvg}
+              alt="skip-next"
+              className="bg-[#F7F7F8] rounded-full p-1 hover:bg-[#EEEEF0] hover:cursor-pointer"
+            />
           </div>
-          <div>
-            <div>00:00.0</div>
-            <span>/</span>
-            <div>00:00.0</div>
+          <div className="flex flex-row justify-center items-center gap-2">
+            <div className="text-xs">00:00.0</div>
+            <span className="text-xs">/</span>
+            <div className="text-xs">00:00.0</div>
           </div>
         </div>
-        <div className="flex flex-row justify-center items-center gap-2">
-          <div>
-            <img src={ZoomOutSvg} alt="zoom-out" />
-            <Slider />
-            <img src={ZoomInSvg} alt="zoom-in" />
+        <div className="flex flex-row justify-center items-center gap-4">
+          <div className="flex flex-row justify-center items-center gap-8">
+            <div className="p-2 rounded-lg hover:bg-[#F7F7F8] hover:cursor-pointer">
+              <img src={ZoomOutSvg} alt="zoom-out" />
+            </div>
+            <Slider
+              value={sliderValue}
+              sx={{
+                width: "100px",
+              }}
+              size="small"
+            />
+            <div className="p-2 rounded-lg hover:bg-[#F7F7F8] hover:cursor-pointer">
+              <img src={ZoomInSvg} alt="zoom-in" />
+            </div>
           </div>
-          <div>|</div>
-          <div>
+          <div className="border-[0.5px] border-[#E1E1E3] h-6" />
+          <div className="p-2 rounded-lg hover:bg-[#F7F7F8] hover:cursor-pointer">
             <img src={TimelineSetting} alt="setting" />
           </div>
         </div>
