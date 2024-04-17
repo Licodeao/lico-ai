8afdb1ee65c8187e9d6de805e705dd37a1c05408
Licodeao <1019805199@qq.com>
2024-04-17 15:41:28 +0800

    feat(Limit): add usage limit entity

--- graduation-backend/src/limit/entities/limit.entity.ts
+++ graduation-backend/src/limit/entities/limit.entity.ts
@@ -1,10 +1,45 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { UserEntity } from 'src/user/entities/user.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class LimitEntity {
+  /**
+   * @field {number} id 次数ID（主键）
+   */
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ comment: '普通用户次数限制', default: 1 })
-  standardLimit: number;
+  /**
+   * @field {number} standardGenerateLimit 普通用户生成次数限制, 默认只有一次
+   */
+  @Column({ comment: '普通用户生成次数限制', default: 1 })
+  standardGenerateLimit: number;
+
+  /**
+   * @field {number} standardExportLimit 普通用户导出次数限制, 默认只有一次
+   */
+  @Column({ comment: '普通用户导出次数限制', default: 1 })
+  standardExportLimit: number;
+
+  /**
+   * @field {number} plusGenerateLimit 付费用户生成次数限制, 默认只有五次
+   */
+  @Column({ comment: '付费用户生成次数限制', default: 5 })
+  plusGenerateLimit: number;
+
+  /**
+   * @field {number} plusExportLimit 付费用户导出次数限制, 默认只有五次
+   */
+  @Column({ comment: '付费用户导出次数限制', default: 5 })
+  plusExportLimit: number;
+
+  @OneToOne(() => UserEntity)
+  @JoinColumn()
+  user: UserEntity;
 }
